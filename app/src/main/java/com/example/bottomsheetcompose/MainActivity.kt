4712cff51219db74b2b080535623e554e6250593
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-07-30 13:15:35 +0700

    add: main screen

--- app/src/main/java/com/example/bottomsheetcompose/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetcompose/MainActivity.kt
@@ -3,27 +3,13 @@ package com.example.bottomsheetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.bottomsheetcompose.ui.theme.BottomSheetComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,36 +30,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen() {
     val scope = rememberCoroutineScope()
-
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-
-    // if skipPartiallyExpanded false, the bottom sheet will show half a screen
-    // if true, the bottom sheet will automatically show fullscreen
-    val bottomSheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = false,
-    )
-
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(18.dp), contentAlignment = Alignment.Center
-    ) {
-        Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { openBottomSheet = !openBottomSheet }) {
-                Text(text = "Open Bottom Sheet")
-            }
-        }
-    }
-
-    if (openBottomSheet) {
-        BottomSheet(onClose = {
-            openBottomSheet = it
-        }, sheetState = bottomSheetState, scope = scope)
-    }
+    BottomSheet(scope = scope)
 }
 
 
